ba130b75930cccf2893881f4d59a7ff8e254a61e
Matt McKenzie <mattmckenzie@hotmail.com>
2017-03-10 12:38:35 +1300

    rebuild from scratch

---
+++ src/app/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-about',
+  templateUrl: 'app/about.component.html',
+//  styleUrls: [ 'app/about.component.css' ]
+})
+export class AboutComponent {
+
+  componentHeader = "public/images/headers/header-tag-about-us.png"
+
+}
